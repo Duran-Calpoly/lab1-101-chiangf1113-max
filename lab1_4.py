fabcca40115568521cc08491de2dea47f5d467c8
chiangf1113-max <chiangf1113@gmail.com>
2026-01-17 01:02:42 +0000

    complete

--- lab1_4.py
+++ lab1_4.py
@@ -1,6 +1,6 @@
-def calculate_average (num1,num1,num2):
+def calculate_average (num1,num2,num3):
     return ((num1+num2+num3)/3)
 def add_tax (bill_total):
     return bill_total*1.1
 def greet_user (name):
-    return "Hello" + name
+    return "Hello " + name
